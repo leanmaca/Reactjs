d91c4410778b768a0b2164f99e3cd9f2d9e3bcfe
leanmaca <leandrinmacalinao@gmail.com>
2022-09-05 16:55:32 +0800

    animallist
    animal list container

--- reactjs/src/Components/AnimalList.js
+++ reactjs/src/Components/AnimalList.js
@@ -1,7 +1,7 @@
 import { QuerySnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import {db} from '../services/firebase-config';
+import { db } from "../services/firebase-config";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import "./css/animallist.css";
 
@@ -12,41 +12,49 @@ function AnimalList() {
   const [animalData, setAnimalData] = useState([]);
 
   useEffect(() => {
-    getAnimals()
+    getAnimals();
   }, []);
 
   useEffect(() => {
     console.log(animalData);
-  }, [animalData])
+  }, [animalData]);
 
-  function getAnimals(){
-    const animalCollectionRef = query(collection(db, "animals"), where("animal_enclosure", "==", enclosure));
-    getDocs(animalCollectionRef).then(response => {
-      const animals = response.docs.map(doc => ({
+  function getAnimals() {
+    const animalCollectionRef = query(
+      collection(db, "animals"),
+      where("animal_enclosure", "==", enclosure)
+    );
+    getDocs(animalCollectionRef).then((response) => {
+      const animals = response.docs.map((doc) => ({
+        id: doc.data().id,
         name: doc.data().animal_name,
         imageUrl: doc.data().animal_imageurl,
         enclosure: doc.data().animal_enclosure,
-      }))
-      setAnimalData(animals)
-    })
+      }));
+      setAnimalData(animals);
+    });
   }
-  
+
   return (
     <div class="animalList">
       <h1>Animal List Page</h1>
       <h2>{location.state.name}</h2>
       <button onClick={() => navigate(-1)}>Back</button>
-      {animalData.map((item, index)=>{
-        return(
-          <div className="list" key={index}>
-            
+      {animalData.map((item, index) => {
+        return (
+          <span
+            onClick={() => {
+              console.log(item.name);
+            }}
+            className="list"
+            key={index}
+          >
             <div class="imagecontainer">
-            <img class="animalimage" src={item.imageUrl}/>
-            
+              <img class="animalimage" src={item.imageUrl} />
             </div>
-            <h3>{item.name}</h3>
-            </div>
-        )
+            {/* <h3>{item.name}</h3> */}
+          </span>
+        );
       })}
     </div>
   );
