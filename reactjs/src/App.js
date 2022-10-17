2655448d1289b2acb9c66d2fe01c54657305eb44
leanmaca <leandrinmacalinao@gmail.com>
2022-10-17 22:01:09 +0800

    Removed Unnecessary Files

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,7 +1,5 @@
 import MapScreen from "./Components/MapScreen";
-import AnimalList from "./Components/AnimalList";
-import AnimalView from "./Components/AnimalView";
-import Quiz from "./Components/Quiz";
+
 import { Route, Switch, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
@@ -10,9 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MapScreen />} />
-          <Route path="/animallist" element={<AnimalList />} />
-          <Route path="/animalview" element={<AnimalView />} />
-          <Route path="/quiz" element={<Quiz />} />
         </Routes>
       </BrowserRouter>
     </div>
